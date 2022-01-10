c2fee0a7d6ef1c43cbc91e2d89039adb1b9ec834
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-01-10 20:49:19 +0000

    added if statement into playGame function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,31 +3,88 @@ document.addEventListener("DOMContentLoaded", function() {
 
 	for (let button of buttons) {
 		button.addEventListener("click", function() {
-			if (this.getAttribute("data-type") === "submit") {
-				checkAnswer();
-			} else {
-				let gameType = this.getAttribute("data-type");
-				runGame(gameType);
-			}
+			if (this.getAttribute("data-type") === "play") {
+				playGame();
+			} 
 		});
     }
 
-function playGame() {
+function playGame(playerCard) {
 
-    let playerCard = document.getElementById('player-screen'); 
+    let playerCard = document.getElementsByTagName('button'); 
+    
+    const cards = ["rock", "paper", "scissors", "lizard", "spock"];
 
-    playerCard.setAttribute('value', playerCard)
+    let computerCard = Math.floor(Math.random() * cards.length);
+    
 
-    const cards = ["rock", "paper", "scissors", "lizzard", "spock"];
-
-    const random = Math.floor(Math.random() * cards.length);
-    console.log(random, cards[random]);
+    if (playerCard === "rock" && computerCard === "scissors") {
+        alert(`${playerCard} beats ${computerCard}. Player gets 1 point.`); 
+    } else if (playerCard === "rock" && computerCard === "lizard") {
+        alert(`${playerCard} beats ${computerCard}. Player gets 1 point.`);    
+    } else if (playerCard === "rock" && computerCard === "paper") {
+        alert(`${playerCard} loses to ${computerCard}. Computer gets 1 point.`);
+    } else if (playerCard === "rock" && computerCard === "spock") {
+        alert(`${playerCard} loses to ${computerCard}. Computer gets 1 point.`);
+    } else if (playerCard === "rock" && computerCard === "rock") {
+        alert(`${playerCard} draws with ${computerCard}. No points awarded to either player.`);
+    }
+    
+    // Paper Scenario for player
+    if (playerCard === "paper" && computerCard === "rock") {
+        alert(`${playerCard} beats ${computerCard}. Player gets 1 point.`);
+    } else if (playerCard === "paper" && computerCard === "spock") {
+        alert(`${playerCard} beats ${computerCard}. Player gets 1 point.`);
+    } else if (playerCard === "paper" && computerCard === "lizard") {
+        alert(`${playerCard} loses to ${computerCard}. Computer gets 1 point.`);
+    } else if (playerCard === "paper" && computerCard === "scissors") {
+        alert(`${playerCard} loses to ${computerCard}. Computer gets 1 point.`);
+    } else if (playerCard === "paper" && computerCard === "paper") {
+        alert(`${playerCard} draws with ${computerCard}. No points awarded to either player.`); 
+    }
+    
+    //Scissors Scenario for player
+    if (playerCard === "scissors" && computerCard === "paper") {
+        alert(`${playerCard} beats ${computerCard}. Player gets 1 point.`);
+    } else if (playerCard === "scissors" && computerCard === "lizard") {
+        alert(`${playerCard} beats ${computerCard}. Player gets 1 point.`);
+    } else if (playerCard === "scissors" && computerCard === "spock") {
+        alert(`${playerCard} loses to ${computerCard}. Computer gets 1 point.`);
+    } else if (playerCard === "scissors" && computerCard === "rock") {
+        alert(`${playerCard} loses to ${computerCard}. Computer gets 1 point.`);
+    } else if (playerCard === "scissors" && computerCard === "scissors") {
+        alert(`${playerCard} draws with ${computerCard}. No points awarded to either player.`); 
+    }
+    
+    //Lizard Scenario for player
+    if (playerCard === "lizard" && computerCard === "spock") {
+        alert(`${playerCard} beats ${computerCard}. Player gets 1 point.`);
+    } else if (playerCard === "lizard" && computerCard === "paper") {
+        alert(`${playerCard} beats ${computerCard}. Player gets 1 point.`);
+    } else if (playerCard === "lizard" && computerCard === "rock") {
+        alert(`${playerCard} loses to ${computerCard}. Computer gets 1 point.`);
+    } else if (playerCard === "lizard" && computerCard === "scissors") {
+        alert(`${playerCard} loses to ${computerCard}. Computer gets 1 point.`);
+    } else if (playerCard === "lizard" && computerCard === "lizard") {
+        alert(`${playerCard} draws with ${computerCard}. No points awarded to either player.`); 
+    }
+    
+    //Spock Scenario for player
+    if (playerCard === "spock" && computerCard === "scissors") {
+        alert(`${playerCard} beats ${computerCard}. Player gets 1 point.`);
+    } else if (playerCard === "spock" && computerCard === "rock") {
+        alert(`${playerCard} beats ${computerCard}. Player gets 1 point.`);
+    } else if (playerCard === "spock" && computerCard === "lizard") {
+        alert(`${playerCard} loses to ${computerCard}. Computer gets 1 point.`);
+    } else if (playerCard === "spock" && computerCard === "paper") {
+        alert(`${playerCard} loses to ${computerCard}. Computer gets 1 point.`);
+    } else if (playerCard === "spock" && computerCard === "spock") {
+        alert(`${playerCard} draws with ${computerCard}. No points awarded to either player.`); 
+    }
  
 }
 
-
-const cards = ["rock", "paper", "scissors", "lizard", "spock"];
-
+/*
 //Have for loop or the above loop to generate a card for player and another for, maybe one of those matrix arrays
 //Assign the result of loop for each player
 //For now, put it in an alert connect to play button (maybe)
@@ -100,7 +157,7 @@ if (playerCard === "spock" && computerCard === "scissors") {
 } else if (playerCard === "spock" && computerCard === "spock") {
     console.log(`${playerCard} draws with ${computerCard}. No points awarded to either player.`); 
 }
-
+*/
 
 
 
@@ -108,4 +165,4 @@ function addScore() {
 
 }
 
-}
+});
