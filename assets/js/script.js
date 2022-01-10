861a7c28fc99bbf414ff7558eab58d012a3ab791
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-01-10 17:53:05 +0000

    add comments for ideas on layout and logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,6 +21,11 @@ function playGame() {
 
 const cards = ["rock", "paper", "scissors", "lizzard", "spock"];
 
+//Have for loop or the above loop to generate a card for player and another for, maybe one of those matrix arrays
+//Assign the result of loop for each player
+//For now, put it in an alert connect to play button (maybe)
+//Have buutons for player card and selection and random loop for compter card
+
 let playerCard = "rock"
 let computerCard = "spock"
 
@@ -50,7 +55,18 @@ if (playerCard === "paper" && computerCard === "rock") {
     console.log(`${playerCard} draws with ${computerCard}. No points awarded to either player.`); 
 }
 
-
+//Scissors Scenario for player
+if (playerCard === "paper" && computerCard === "rock") {
+    console.log(`${playerCard} beats ${computerCard}. Player gets 1 point.`);
+} else if (playerCard === "paper" && computerCard === "spock") {
+    console.log(`${playerCard} beats ${computerCard}. Player gets 1 point.`);
+} else if (playerCard === "paper" && computerCard === "lizzard") {
+    console.log(`${playerCard} loses to ${computerCard}. Computer gets 1 point.`);
+} else if (playerCard === "paper" && computerCard === "scissors") {
+    console.log(`${playerCard} loses to ${computerCard}. Computer gets 1 point.`);
+} else if (playerCard === "paper" && computerCard === "paper") {
+    console.log(`${playerCard} draws with ${computerCard}. No points awarded to either player.`); 
+}
 
 
 function addScore() {
