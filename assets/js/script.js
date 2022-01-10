124c7127dfed140434ae1b374d77f11b9c100ecc
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-01-10 16:58:07 +0000

    add rock sceanrio if statement to be tested

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,14 +22,32 @@ function playGame() {
 const cards = ["rock", "paper", "scissors", "lizzard", "spock"];
 
 let playerCard = "rock"
-let computerCard = "scissors"
+let computerCard = "spock"
 
-if (playerCard === "rock" && computerCard === "scissors" || "lizzard") {
-    console.log(`${playerCard} beats ${computerCard}. Player gets 1 point.`);
-} else if (playerCard === "rock" && computerCard === "paper" || "spock") {
+// Rock Scenario for player
+if (playerCard === "rock" && computerCard === "scissors") {
+    console.log(`${playerCard} beats ${computerCard}. Player gets 1 point.`); 
+} else if (playerCard === "rock" && computerCard === "lizzard") {
+    console.log(`${playerCard} beats ${computerCard}. Player gets 1 point.`);    
+} else if (playerCard === "rock" && computerCard === "paper") {
+    console.log(`${playerCard} loses to ${computerCard}. Computer gets 1 point.`);
+} else if (playerCard === "rock" && computerCard === "spock") {
     console.log(`${playerCard} loses to ${computerCard}. Computer gets 1 point.`);
 } else if (playerCard === "rock" && computerCard === "rock") {
     console.log(`${playerCard} draws with ${computerCard}. No points awarded to either player.`);
+}
+
+// Paper Scenario for player
+if (playerCard === "paper" && computerCard === "rock") {
+    console.log(`${playerCard} beats ${computerCard}. Player gets 1 point.`);
+} else if (playerCard === "paper" && computerCard === "spock") {
+    console.log(`${playerCard} beats ${computerCard}. Player gets 1 point.`);
+} else if (playerCard === "paper" && computerCard === "lizzard") {
+    console.log(`${playerCard} loses to ${computerCard}. Computer gets 1 point.`);
+} else if (playerCard === "paper" && computerCard === "scissors") {
+    console.log(`${playerCard} loses to ${computerCard}. Computer gets 1 point.`);
+} else if (playerCard === "paper" && computerCard === "paper") {
+    console.log(`${playerCard} draws with ${computerCard}. No points awarded to either player.`); 
 }
 
 
